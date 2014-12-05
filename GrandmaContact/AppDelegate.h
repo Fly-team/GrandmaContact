9c7c1cb53fef310645ed27536939eebe373791e4
apple <apple@applematoMacBook-Air.local>
2014-12-05 16:15:52 +0800

    Initial Commit

---
+++ GrandmaContact/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GrandmaContact
+//
+//  Created by apple on 14/12/5.
+//  Copyright (c) 2014年 fly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
